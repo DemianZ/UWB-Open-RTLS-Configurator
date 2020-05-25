7507e378680e19b27ce551a1ebcf91c48615f166
DemianZ <zenkov94@mail.ru>
2020-05-25 23:33:27 +0300

    clean fbs

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,6 +1,6 @@
-from fbs_runtime.application_context.PyQt5 import ApplicationContext
+# from fbs_runtime.application_context.PyQt5 import ApplicationContext
+from PyQt5.QtWidgets import QApplication, QWidget
 import sys
-import os
 import logging as log
 
 from design_components.MainWindow import MainWindow
@@ -11,11 +11,11 @@ log.basicConfig(level=log.DEBUG,
 log.getLogger('matplotlib.font_manager').disabled = True
 
 if __name__ == '__main__':
-    app_context = ApplicationContext()       # 1. Instantiate ApplicationContext
+
+    app = QApplication(sys.argv)
+
     window = MainWindow()
     window.start_tasks()
     window.show()
-    exit_code = app_context.app.exec_()      # 2. Invoke appctxt.app.exec_()
-    window.stop_tasks()
-    os._exit(0)
-    sys.exit(exit_code)
+
+    sys.exit(app.exec_())
